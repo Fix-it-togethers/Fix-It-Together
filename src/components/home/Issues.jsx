7107a3b8d9a886cd7cca5e207d23cdbca04bfb3b
Miirshe <miirshe@gmail.com>
2023-08-12 21:11:48 +0300

    updated posts of details to view when each of post card  to click and also like comments , likes and also shares

--- src/components/home/Issues.jsx
+++ src/components/home/Issues.jsx
@@ -1,10 +1,6 @@
 import { CiFlag1 } from "react-icons/ci";
 import { CgCalendarDates } from "react-icons/cg";
-import {
-  AiOutlineDislike,
-  AiOutlineLike,
-  AiOutlineComment,
-} from "react-icons/ai";
+import { AiOutlineDislike, AiOutlineLike , AiOutlineComment} from "react-icons/ai";
 import {
   BiSearch,
   BiSolidLeftArrowSquare,
@@ -15,8 +11,8 @@ import { useFetchIssueQuery } from "../../store/Api/FixSlice";
 import { useState } from "react";
 import { BiShare } from "react-icons/bi";
 function Issues({ handlelikes }) {
-  const [InputSearch, setInputSearch] = useState("");
-  const [opps, setOpps] = useState(false);
+  const [InputSearch, setInputSearch] = useState('');
+  const [opps , setOpps] = useState(false);
   const { data: fetchIssue = [] } = useFetchIssueQuery();
   console.log("miirshe dhib badnidaa", fetchIssue);
   const [currentPage, setCurrentPage] = useState(1);
@@ -26,10 +22,10 @@ function Issues({ handlelikes }) {
   const records = fetchIssue?.slice(firstIndex, lastIndex);
   const numberPage = Math.ceil(fetchIssue?.length / recordPerge);
   const numbers = [...Array(numberPage + 1).keys()].slice(1);
-  const handleInput = (e) => {
+  const handleInput = (e)=>{
     setInputSearch(e.target.value);
     setOpps(true);
-  };
+  }
   return (
     <div className="mt-14 w-[90%] mx-auto lg:w-[90%] xl:w-[80%] text-[#032443] bg-[#F7F5F5] p-2">
       <div className="mt-2">
@@ -41,7 +37,7 @@ function Issues({ handlelikes }) {
             <input
               className=" w-full p-2 py-2 px-10 rounded shadow border-[#032443] "
               type="text"
-              onChange={(e) => handleInput(e)}
+              onChange={(e)=>handleInput(e)}
             />{" "}
             <BiSearch
               className="text-base mt-3 inline absolute right-[12%]"
@@ -50,82 +46,71 @@ function Issues({ handlelikes }) {
           </div>
         </div>
         <div className="mt-10 w-full grid grid-cols-1 lg:grid-cols-2 gap-2">
-          {records
-            ?.filter((issue) => {
-              return issue.location
-                .toLowerCase()
-                .includes(InputSearch.toLowerCase());
-            })
-            .map((issue) => {
-              return (
-                <Link
-                  className="w-full mx-auto lg:mx-0 flex flex-col lg:flex-row justify-start items-start gap-2 lg:justify-start lg:items-start transition hover:shadow-lg cursor-pointer ease-in-out p-1"
-                  to={`/Detail/${issue.id}`}
-                  key={issue.id}
-                >
+          {records?.filter(issue=>{
+            return issue.location.toLowerCase().includes(InputSearch.toLowerCase());
+          }).map((issue) => {
+            return (
+              <Link 
+                className="w-full mx-auto lg:mx-0 flex flex-col lg:flex-row justify-start items-start gap-2 lg:justify-start lg:items-start transition hover:shadow-lg cursor-pointer ease-in-out p-1"
+                key={issue.id}
+                to={`/Detail/${issue.id}`}
+              >
+                <Link className="w-full" to={`/Detail/${issue.id}`}>
                   <img
                     className="w-full lg:w-[90%] h-64 object-cover"
                     src={issue.image}
                     alt="photo fix issue"
                   />
-
-                  <div className=" space-y-3 lg:mt-0 mt-2 w-full">
-                    <h1 className="text-base font-medium">
-                      {issue.title.slice(0, 50) +
-                        [issue.title.length >= 50 ? "..." : " "]}
-                    </h1>
-                    <p className=" text-sm w-full">
-                      {issue.description.slice(0, 150) +
-                        [
-                          issue.description.length > 150
-                            ? "  Read more..."
-                            : " ",
-                        ]}{" "}
-                    </p>
-                    <div className="flex flex-col justify-start items-start space-y-1 gap-1">
-                      <p className="flex flex-row justify-start lg:items-center items-start">
-                        <CiFlag1 className="mt-1" size={18} />
-                        <span className="ml-2 text-xs">{issue.location}</span>
-                      </p>
-                      <p className="flex flex-row justify-start lg:items-center items-start">
-                        <CgCalendarDates className="mt-1" size={18} />
-                        <span className="ml-2 text-xs">{issue.date}</span>
-                      </p>
-                    </div>
-
-                    <div className="flex flex-row justify-between items-start lg:items-center space-y-1 border-t-2 w-full p-1 ">
-                      <span className="flex flex-row justify-start items-center space-x-6">
-                        <p>
-                          <AiOutlineLike
-                            className="inline"
-                            onClick={() => handlelikes()}
-                            size={20}
-                          />{" "}
-                          <span>{issue.upvotes > 0 ? issue.upvotes : ""}</span>
-                        </p>
-                        <p>
-                          <AiOutlineDislike
-                            onClick={() => handlelikes()}
-                            size={20}
-                            className="inline"
-                          />
-                          <span>
-                            {issue.downvotes > 0 ? issue.downvotes : ""}
-                          </span>
-                        </p>
-                        <small>
-                          <BiShare className="inline" size={20} />
-                        </small>
-
-                        <small>
-                          <AiOutlineComment className="inline" size={20} />
-                        </small>
-                      </span>
-                    </div>
-                  </div>
                 </Link>
-              );
-            })}
+                <div className=" space-y-3 lg:mt-0 mt-2 w-full">
+                  <h1 className="text-base font-medium">
+                    {issue.title.slice(0, 50) +
+                      [issue.title.length >= 50 ? "..." : " "]}
+                  </h1>
+                  <p className=" text-sm w-full">
+                    {issue.description.slice(0, 150) +
+                      [
+                        issue.description.length > 150 ? "  Read more..." : " ",
+                      ]}{" "}
+                  </p>
+                  <div className="flex flex-col justify-start items-start space-y-1 gap-1">
+                    <p className="flex flex-row justify-start lg:items-center items-start">
+                      <CiFlag1 className="mt-1" size={18} />
+                      <span className="ml-2 text-xs">{issue.location}</span>
+                    </p>
+                    <p className="flex flex-row justify-start lg:items-center items-start">
+                      <CgCalendarDates className="mt-1" size={18} />
+                      <span className="ml-2 text-xs">{issue.date}</span>
+                    </p>
+                  </div>
+
+                  <div className="flex flex-row justify-between items-start lg:items-center space-y-1 border-t-2 w-full p-1 ">
+                    <span className="flex flex-row justify-start items-center space-x-6">
+                      <p>
+                        <AiOutlineLike className="inline" onClick={() => handlelikes()} size={20} /> <span>{issue.upvotes > 0 ? issue.upvotes : '' }</span>
+                      </p>
+                      <p>
+                      <AiOutlineDislike
+                        onClick={() => handlelikes()}
+                        size={20}
+                        className="inline"
+                      />
+                      <span>{issue.downvotes > 0 ? issue.downvotes :'' }</span>
+                      </p>
+                      <small>
+                      <BiShare className="inline" size={20}/>
+                      </small>
+
+                      <small>
+                       <AiOutlineComment className="inline" size={20}/>
+                      </small>
+
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </div>
       <nav className="mt-20">
